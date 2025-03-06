6f4170e0a9e8fd6ef58f8b5f1bffea847d0a6972
Flaquima <flaquima@gmail.com>
2025-03-06 17:55:28 -0600

    primer codigo

--- app.js
+++ app.js
@@ -1,1 +1,52 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let inputAmigo = document.getElementById("amigo");
+    let nombreAmigo = inputAmigo.ariaValueMax.trim();
+
+    if (nombre === ""){
+        alert ("Por favor, inserte un Nombre.");
+        return;
+    }
+
+    if(amigos.incluides(nombreAmigo)){
+        alert (`El nombre ${nombreAmigo} Ya esta en la lista`);
+        return;
+    }
+
+    amigos.push(nombreAmigo);
+
+    inputAmigo.value = "";
+    inputAmigo.focus();
+
+    actualizarLista();
+
+}
+
+function actualizarLista() {
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
+
+    for (let i = 0; i<amigos.length; i++){
+        const li = document.createElement("li");
+        li.textContent = amigos[i];
+        listaAmigos.appendChild(li);
+    }
+}
+
+function sortearAmigo() {
+    if(amigos.length === 0){
+        alert ("No hay amigos disponibles para sortear. Agrega al menos uno ");
+        return;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random()*amigos.length);
+
+    let amigoSorteado = amigos(indiceAleatorio);
+
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `Amigo sorteado: <strong>${amigoSorteado}<strong>`;
+
+
+}
